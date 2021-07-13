b0ed5b3100eba1b7f480cd8dd7bdcb57a9d7f129
sworc <lightbox@nifty.com>
2021-07-13 18:55:42 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace cs_console_framework_open_file_dialog
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            OpenFileDialog obj = new OpenFileDialog();
+            obj.Filter = "PHP|*.php";
+            obj.InitialDirectory = @"C:\";
+
+            if (obj.ShowDialog() != DialogResult.OK){
+                return;
+            }
+
+            try
+            {
+                StreamReader sr = new StreamReader(obj.FileName, Encoding.GetEncoding("utf-8"));
+
+                string text;
+
+                // 内容を一気に読み込む
+                Debug.Write( text = sr.ReadToEnd() ); 
+
+                sr.Close();
+
+            }
+            catch (Exception ex)
+            {
+
+                string error = ex.ToString();
+                MessageBox.Show(error);
+                return;
+
+            }
+        }
+    }
+}
